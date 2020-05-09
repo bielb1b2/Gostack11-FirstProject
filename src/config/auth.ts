46be26ec57033fdbcf760d85629f1359152fe7c7
bielb1b2 <gabriel.vitinho@Hotmail.com>
2020-05-09 05:19:07 -0300

    nivel 02 aula de uploads

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '0e80ebc968566ac2fe7d2d67cb335cca',
+        expiresIn: '1d',
+    },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,10 +4,13 @@ import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
 // SoC: Separation of Concerns (Separção de preocupações)
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
@@ -18,7 +21,7 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
     try {
-        const { provider, date } = request.body;
+        const { provider_id, date } = request.body;
 
         const parsedDate = parseISO(date);
 
@@ -26,7 +29,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
         const appointment = await createAppointment.execute({
             date: parsedDate,
-            provider,
+            provider_id,
         });
 
         return response.json(appointment);
