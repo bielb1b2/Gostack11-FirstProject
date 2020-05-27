8056b3661529d90d788c7f2cd83f47e49d6239db
bielb1b2 <gabriel.vitinho@Hotmail.com>
2020-05-27 17:22:04 -0300

    configurando context toast

--- frontend/src/pages/signIn/index.tsx
+++ frontend/src/pages/signIn/index.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -21,6 +22,7 @@ const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
     const { signIn } = useAuth();
+    const { addToast } = useToast();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
@@ -38,18 +40,21 @@ const SignIn: React.FC = () => {
                     abortEarly: false,
                 });
 
-                signIn({
+                await signIn({
                     email: data.email,
                     password: data.password,
                 });
             } catch (err) {
                 if (err instanceof Yup.ValidationError) {
                     const errors = getValidationErrors(err);
+
                     formRef.current?.setErrors(errors);
                 }
+
+                addToast();
             }
         },
-        [signIn],
+        [signIn, addToast],
     );
 
     return (
